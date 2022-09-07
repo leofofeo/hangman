d27b4eb4b70cbb1b9a807ee023a066ff814bc661
leorubiano <leo>
2022-09-07 16:46:44 -0500

    test for game won or lost

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -1,8 +1,9 @@
 defmodule Hangman.Impl.Game do
 
+  alias Hangman.Type
   @type t :: %Hangman.Impl.Game{
     turns_left: integer,
-    game_state: Hangman.state,
+    game_state: Type.state,
     letters: list(String.t),
     used: MapSet.t(String.t),
   }
@@ -13,10 +14,27 @@ defmodule Hangman.Impl.Game do
     used: MapSet.new()
   )
 
-  def new_game do
-    %Hangman.Impl.Game{
-      letters: Dictionary.random_word |> String.codepoints
+
+  @spec new_game() :: t
+  def new_game, do: new_game(Dictionary.random_word)
+
+  @spec new_game(String.t) :: t
+  def new_game(word) do
+    %__MODULE__{
+      letters: word |> String.codepoints
     }
   end
 
+  @spec make_move(t, String.t) :: { t, Type.Tally}
+  def make_move(game = %{ game_state: state}, _guess) when state in [:won, :lost], do: { game, tally(game)}
+
+  @spec tally(t) :: Type.Tally
+  defp tally(game) do
+    %{
+      turns_left: game.turns_left,
+      game_state: game.game_state,
+      letters: [],
+      used: game.used |> MapSet.to_list |> Enum.sort,
+    }
+  end
 end
---
+++ hangman/test/impl_game_test.exs
@@ -0,0 +1,27 @@
+defmodule HangmanImplGameTest do
+  use ExUnit.Case
+  alias Hangman.Impl.Game
+
+  test "new game returns structure" do
+    game = Game.new_game
+
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert length(game.letters) > 0
+  end
+
+  test "new game returns correct word" do
+    game = Game.new_game("wombat")
+    assert game.letters == ["w", "o", "m", "b", "a", "t"]
+  end
+
+  test "state doesn't change if a game is won or lost" do
+    for state <- [:won, :lost] do
+      game = Game.new_game("wombat")
+      game = Map.put(game, :game_state, state)
+      { new_game, _tally} = Game.make_move(game, "x")
+      assert new_game == game
+    end
+
+  end
+end
